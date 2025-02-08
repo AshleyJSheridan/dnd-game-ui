f9427a62094f9a355c20af1efa80c48a5bb99037
Ashley Sheridan <ash@ashleysheridan.co.uk>
2025-02-08 19:42:11 +0000

    added header logo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {CharactersNavComponent} from '../character/characters-nav/characters-nav.component';
+import {LogoFullComponent} from '../logo-full/logo-full.component';
 
 @Component({
   selector: 'app-header',
     imports: [
-        CharactersNavComponent
+        CharactersNavComponent,
+        LogoFullComponent
     ],
   templateUrl: './header.component.html'
 })
---
+++ src/app/components/logo-full/logo-full.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+      selector: 'app-logo-full',
+      imports: [],
+      templateUrl: './logo-full.component.html'
+})
+export class LogoFullComponent {
+
+}
