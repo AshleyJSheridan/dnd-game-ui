7ec1dd8fb077de1b70391c3734e09ec5f6a0d67c
Ashley Sheridan <ash@ashleysheridan.co.uk>
2025-02-21 17:21:17 +0000

    fixed indentation

--- src/app/components/icons/char-class-icon/char-class-icon.component.ts
+++ src/app/components/icons/char-class-icon/char-class-icon.component.ts
@@ -1,9 +1,9 @@
 import {Component, input, InputSignal} from '@angular/core';
 
 @Component({
-  selector: 'app-char-class-icon',
-  imports: [],
-  templateUrl: './char-class-icon.component.html'
+    selector: 'app-char-class-icon',
+    imports: [],
+    templateUrl: './char-class-icon.component.html'
 })
 export class CharClassIconComponent {
     readonly charClassName: InputSignal<string|undefined> = input<string>();
