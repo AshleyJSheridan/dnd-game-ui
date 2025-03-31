e64bd84cc945939cb1669020f29ce0da519a56dc
Ashley Sheridan <ash@ashleysheridan.co.uk>
2025-03-31 19:55:37 +0100

    added grid option, with size and colour options

--- src/app/components/campaigns/maps-view/maps-view.component.ts
+++ src/app/components/campaigns/maps-view/maps-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal, WritableSignal} from '@angular/core';
 import {HeaderComponent} from '../../header/header.component';
 import {HeaderSlimComponent} from '../../header/header-slim/header-slim.component';
 import {CampaignService} from '../../../services/campaign-service';
@@ -6,13 +6,15 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Campaign} from '../../../entities/Campaign';
 import {CampaignMap} from '../../../entities/CampaignMap';
 import {MapActionIconComponent} from '../../icons/map-action-icon/map-action-icon.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
     selector: 'app-maps-view',
     imports: [
         HeaderComponent,
         HeaderSlimComponent,
-        MapActionIconComponent
+        MapActionIconComponent,
+        FormsModule
     ],
     templateUrl: './maps-view.component.html'
 })
@@ -20,6 +22,10 @@ export class MapsViewComponent {
     private readonly route = inject(ActivatedRoute);
     campaignMap: CampaignMap | undefined = undefined;
     mapMode: string = 'Pointer';
+    gridSize: number = 100;
+    showGrid: boolean = false;
+    gridColour: WritableSignal<string> = signal('#ffffff');
+    showSettings: boolean = false;
 
     constructor(private campaignService: CampaignService, private router: Router) {}
 
@@ -47,6 +53,31 @@ export class MapsViewComponent {
     }
 
     setMapMode(mode: string): void {
+        this.showSettings = true;
         this.mapMode = mode;
+    }
+
+    getViewBoxWidth(): number {
+        return Math.ceil((this.campaignMap?.width ?? 1) / this.gridSize) * this.gridSize;
+    }
+
+    getViewBoxHeight(): number {
+        return Math.ceil((this.campaignMap?.height ?? 1) / this.gridSize) * this.gridSize;
+    }
+
+    changeGridSize(size: number): void {
+        if (this.gridSize + size === 0)
+            return;
+
+        // TODO make API call to set grid size for this map
+        this.gridSize += size;
+    }
+
+    saveGridColour(): void {
+        // TODO make API call to set grid size for this map
+    }
+
+    hideSettings(): void {
+        this.showSettings = false;
     }
 }
