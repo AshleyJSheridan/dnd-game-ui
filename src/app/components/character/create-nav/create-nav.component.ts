53e8abff454627622176b30e8dcc96ab6e833bc9
Ashley Sheridan <ash@ashleysheridan.co.uk>
2025-04-19 17:01:30 +0100

    set active states correctly for char create nav links

--- src/app/components/character/create-nav/create-nav.component.ts
+++ src/app/components/character/create-nav/create-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CharacterService } from '../../../services/character.service';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-create-nav',
@@ -11,11 +12,18 @@ import { CharacterService } from '../../../services/character.service';
     templateUrl: './create-nav.component.html'
 })
 export class CreateNavComponent {
+    //private readonly route = inject(ActivatedRoute);
+
     charGuid: string = '';
 
-    constructor(public characterService: CharacterService) {}
+    constructor(public characterService: CharacterService, private location: Location) {}
 
     ngOnInit() {
         this.charGuid = this.characterService.getCharGuid();
+    }
+
+    // unfortunately, ActivatedRoute is all kinds of broken when working with child components, so this is the workaround
+    isLinkActive(path: string): boolean {
+        return this.location.path().includes(`edit/${path}`);
     }
 }
